40ccc6a6ed610528ec98e1be2a3728817282a2e7
ElamaranDev <marandev007@gmail.com>
2023-06-21 19:41:08 +0530

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 setInterval(() => {
   setClock();
 }, 1000);
+
 const deg = 6;
 const hourHand = document.querySelector("#hr");
 const minuteHand = document.querySelector("#min");
 const secondHand = document.querySelector("#sec");
+const togglebtn = document.querySelector(".toggle-btn");
+
+togglebtn.addEventListener("click", () => {
+  toggleTheme();
+});
+
+function toggleTheme() {
+  const body = document.querySelector("body");
+  if(!body.classList.contains("Light")){
+    body.classList.add("Light");
+  }
+  else{
+    body.classList.remove("Light");
+  }
+  
+}
 
 function setClock() {
   let time = new Date();
@@ -15,6 +32,5 @@ function setClock() {
   hourHand.style.transform = `rotateZ(${hr + min / 12}deg)`;
   minuteHand.style.transform = `rotateZ(${min}deg)`;
   secondHand.style.transform = `rotateZ(${sec}deg)`;
-
 }
 setClock();
